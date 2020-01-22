1fc734e1db07a857eb219c42d765f2f5e4c79b62
Ariadna Pulido <adna.pulido@gmail.com>
2020-01-22 18:43:00 -0500

    unfinished user registration with sql server aspx

---
+++ dbo.UserRegistration.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(50) NULL, 
+    [LastName] VARCHAR(50) NULL, 
+    [Username] VARCHAR(50) NULL, 
+    [Password] VARCHAR(50) NULL
+)
